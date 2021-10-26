17db63d0b1b3acb01e326fbdab10a2328c4cfd73
natewong <nwwong17@gmail.com>
2021-10-25 22:54:48 -0700

    Created a BankAccount class for APCS CW

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,6 +1,40 @@
 public class BankAccount {
     // Instance variables
-    private int balance;
+    private int balance=50;
     private int accountNumber;
     private String preferredName;
+
+    public BankAccount(int a, String p) {
+        accountNumber = a;
+        preferredName = p;
+    }
+
+    public Boolean withdrawl(int amount) //changes amount of money in bank, returns false if amount cannot be withdrawn
+    {
+        this.balance -= amount;
+        return this.balance >= 0;
+    }
+
+    public void deposit(int amount) {
+        this.balance += amount;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public String getPreferredName() {
+        return this.preferredName;
+    }
+    public void setPreferredName(String s){
+        this.preferredName=s;
+    }
+    @Override
+    public String toString() {
+        return this.accountNumber + " is your account number. $" + this.balance + " is your current balance.";
+    }
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -6,7 +6,35 @@ public class BankClient {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your preferred name?");
         String name = scan.nextLine();
-//        // Create a bank account with the given name and the account number 101134
-//        BankAccount account = new BankAccount(name, 101134);
+        BankAccount account = new BankAccount((int) (Math.random()*10000), name);
+        System.out.println(account);
+        Boolean doneWithInteraction = false;
+        while(!doneWithInteraction){
+            name = account.getPreferredName();
+            System.out.println("Hi, " + name + ". What would you like to do? (withdraw/deposit/check balance/check account number/set preferred name/print summary/leave)");
+            String choice = scan.nextLine().toLowerCase();
+            System.out.println("Option = " + choice);
+            if(choice.equals("leave")) {
+                System.out.println("Have a nice day!");
+                doneWithInteraction=true;
+            }
+            else if(choice.equals("withdraw")) {
+                System.out.println("Enter a value to withdraw");
+                int withAmount = scan.nextInt();
+                String placehold = scan.nextLine();
+                Boolean positiveAmount = account.withdrawl(withAmount);
+                if(positiveAmount) {
+                    System.out.println("You now have $" + account.getBalance() + " in your bank");
+                }
+                else{
+                    System.out.println("You are in debt because you withdrew too much!");
+                    doneWithInteraction=true;
+                }
+            }
+            else{
+                System.out.println("The bank was left unfinished, therefore these options do not exist");
+                doneWithInteraction=true;
+            }
+        }
     }
 }
